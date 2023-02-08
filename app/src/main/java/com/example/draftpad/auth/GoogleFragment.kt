5bebf2081e15a466c3e5266a67f18469d0e12bcd
MahiSharma000 <sharma02Mahi@gmail.com>
2023-02-08 21:20:01 +0530

    added few files

---
+++ app/src/main/java/com/example/draftpad/auth/GoogleFragment.kt
@@ -0,0 +1,28 @@
+package com.example.draftpad.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.draftpad.R
+
+class GoogleFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_google, container, false)
+    }
+
+    companion object {
+
+    }
+}
