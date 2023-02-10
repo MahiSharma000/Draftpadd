0a70c27742b5b8770d0e89135ec9215b1ae88107
Preeti <negipreeti2003@gmail.com>
2023-02-10 11:21:11 +0530

    changed

--- app/src/main/java/com/example/draftpad/auth/AuthFragment.kt
+++ app/src/main/java/com/example/draftpad/auth/AuthFragment.kt
@@ -35,7 +35,7 @@ class AuthFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth2, container, false)
+        return inflater.inflate(R.layout.fragment_auth, container, false)
     }
 
     companion object {
