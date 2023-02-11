c4d0441d6c5cf0cf88d61fc59db1911fb01a6b79
MahiSharma000 <sharma02Mahi@gmail.com>
2023-02-11 16:27:50 +0530

    edited layout

--- app/src/main/java/com/example/draftpad/auth/FirstActivity.kt
+++ app/src/main/java/com/example/draftpad/auth/FirstActivity.kt
@@ -2,6 +2,7 @@ package com.example.draftpad.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.draftpad.R
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
