e44cf3df90a02b135aa756ef522fc5512417ca82
Preeti <negipreeti2003@gmail.com>
2023-02-11 12:46:17 +0530

    updated email and phone Login

--- app/src/main/java/com/example/draftpad/auth/AuthFragment.kt
+++ app/src/main/java/com/example/draftpad/auth/AuthFragment.kt
@@ -6,22 +6,58 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.draftpad.R
+import android.content.Intent
+import androidx.navigation.fragment.findNavController
+import com.example.draftpad.MainActivity
+import com.example.draftpad.databinding.FragmentAuthBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class AuthFragment : Fragment() {
 
-
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(auth.currentUser!=null){
+            startActivity(Intent(activity, MainActivity::class.java))
+            activity?.finish()
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_e_p_login, container, false)
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            imgEmail.setOnClickListener {
+                findNavController().navigate(R.id.action_authFragment_to_EPLoginFragment2)
+            }
+            imgPhone.setOnClickListener {
+                findNavController().navigate(R.id.action_authFragment_to_phoneLoginFragment2)
+            }
+            imgGoogle.setOnClickListener {
+                findNavController().navigate(R.id.action_authFragment_to_googleFragment2)
+            }
+        }
+
     }
 
     companion object {
