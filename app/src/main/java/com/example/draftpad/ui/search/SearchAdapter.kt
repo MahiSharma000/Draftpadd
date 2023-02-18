846587c486a46e23634f227dd91d7aa39203be6e
MahiSharma000 <sharma02Mahi@gmail.com>
2023-02-18 12:58:53 +0530

    changed layout

--- app/src/main/java/com/example/draftpad/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/example/draftpad/ui/search/SearchAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.draftpad.ui.search
 
-class SearchAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.draftpad.ui.library.ReadingListFragment
+
+class SearchAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        if(position== 0){
+            return BooksFragment()
+        }
+        else if (position == 1){
+            return ProfileFragment()
+        }
+            return ReadingListFragment()
+    }
 }
