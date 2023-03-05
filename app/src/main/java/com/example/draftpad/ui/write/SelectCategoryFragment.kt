7f9aa6badedc4b5f633d97d062544d1ecea123ff
MahiSharma000 <sharma02Mahi@gmail.com>
2023-03-05 12:21:21 +0530

    updated

--- app/src/main/java/com/example/draftpad/ui/write/SelectCategoryFragment.kt
+++ app/src/main/java/com/example/draftpad/ui/write/SelectCategoryFragment.kt
@@ -1,13 +1,24 @@
 package com.example.draftpad.ui.write
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.draftpad.R
+import com.example.draftpad.databinding.FragmentSelectCategoryBinding
+import com.example.draftpad.ui.search.CategoryAdapter
+import com.example.draftpad.ui.search.SearchFragmentDirections
+import com.example.draftpad.ui.search.SearchViewModel
 
 class SelectCategoryFragment : Fragment() {
+    private var _binding: FragmentSelectCategoryBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: SearchViewModel by activityViewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +29,25 @@ class SelectCategoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_category, container, false)
+        _binding = FragmentSelectCategoryBinding.inflate(inflater)
+        binding.editCategoryViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+            viewModel.categories.observe(viewLifecycleOwner) { categories ->
+                this.rveditcategory.layoutManager = LinearLayoutManager(context)
+                this.rveditcategory.adapter = CategoryAdapter() { category ->
+                    viewModel.setCategory(category)
+                }
+                Log.e("SearchFragment", categories.toString())
+                (binding.rveditcategory.adapter as CategoryAdapter).submitList(categories)
+            }
+        }
     }
 
 }
