dc34996bbe2e13799931b45b809e5c48bfa96fac
MahiSharma000 <sharma02Mahi@gmail.com>
2023-03-15 20:52:36 +0530

    updated

--- app/src/main/java/com/example/draftpad/network/Chapter.kt
+++ app/src/main/java/com/example/draftpad/network/Chapter.kt
@@ -6,8 +6,6 @@ data class Chapter(
     val id: Int = 1,
     @Json(name = "title") val title: String,
     @Json(name = "book_id") val book_Id: Int,
-    @Json(name = "book_title") val book_title: String,
-    @Json(name = "book_views") val book_views: Int,
     @Json(name = "category_id") val category_id: Int,
     @Json(name = "user_id") val user_Id: Int,
     @Json(name = "content") val content: String,
--- app/src/main/java/com/example/draftpad/ui/write/WriteViewModel.kt
+++ app/src/main/java/com/example/draftpad/ui/write/WriteViewModel.kt
@@ -1,17 +1,75 @@
 package com.example.draftpad.ui.write
 
 import android.net.Uri
+import android.os.Build
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.draftpad.network.ApiClient
+import com.example.draftpad.network.Chapter
+import com.example.draftpad.network.PostChapterResponse
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+
+enum class WriteApiStatus { LOADING, ERROR, DONE, NONE }
 
 class WriteViewModel : ViewModel() {
-    fun setImageUri(uri: Uri?, b: Boolean) {
 
+    private val _status = MutableLiveData<WriteApiStatus>()
+    val status: MutableLiveData<WriteApiStatus> = _status
+
+    private val _response = MutableLiveData<PostChapterResponse>()
+    val response: MutableLiveData<PostChapterResponse> = _response
+
+    init {
+        _status.value = WriteApiStatus.NONE
     }
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    fun createNewChapter(bookId: Int, chapterTitle: String, chapterContent: String) {
+        try {
+            val currDateTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                LocalDateTime.now()
+            } else {
+                System.currentTimeMillis()
+            }
+            val chapter=Chapter(
+                id = 1,
+                book_Id = bookId!!,
+                title = chapterTitle!!,
+                content = chapterContent!!,
+                created_at = currDateTime.toString(),
+                updated_at = currDateTime.toString(),
+                category_id = 1,
+                status = 0,
+                total_comments = 0,
+                total_likes = 0,
+                user_Id = 1,
+            )
+            postChapter(chapter)
+        }catch (e:Exception){
+            Log.d("Error", "createnewChapter: ${e.message}")
+        }
     }
-    val text: LiveData<String> = _text
+
+    private fun postChapter(chapter: Chapter) {
+        viewModelScope.launch {
+            _status.value = WriteApiStatus.LOADING
+            _response.value = ApiClient.retrofitService.createChapter(
+                title = chapter.title,
+                book_id = chapter.book_Id,
+                content = chapter.content,
+                category_id = chapter.category_id,
+                user_id = chapter.user_Id,
+                status = chapter.status,
+                created_at = chapter.created_at,
+                updated_at = chapter.updated_at,
+                total_comments = chapter.total_comments,
+                total_likes = chapter.total_likes,
+            )
+            _status.value = WriteApiStatus.DONE
+        }
+    }
+
 }
