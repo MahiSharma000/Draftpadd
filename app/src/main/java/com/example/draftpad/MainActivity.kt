21ba1e598736aaab18b90cd0a7ea68f0dc8d36b7
MahiSharma000 <sharma02Mahi@gmail.com>
2023-04-07 18:24:21 +0530

    updated

--- app/src/main/java/com/example/draftpad/MainActivity.kt
+++ app/src/main/java/com/example/draftpad/MainActivity.kt
@@ -1,26 +1,17 @@
 package com.example.draftpad
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.draftpad.auth.SharedPreferenceManager
 import com.example.draftpad.databinding.ActivityMainBinding
-import com.example.draftpad.setting.SettingsFragment
-import com.example.draftpad.ui.home.HomeFragment
-import com.example.draftpad.ui.notifications.NotificationsFragment
-import com.example.draftpad.ui.premium.PremiumFragment
-import com.example.draftpad.ui.profile.UserProfileFragment
-import com.example.draftpad.ui.write.WriteFragment
-import com.google.android.material.navigation.NavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         sharedPreferencesManager = SharedPreferenceManager(this)
-        if(!sharedPreferencesManager.isLoggedIn()){
+        if (!sharedPreferencesManager.isLoggedIn()) {
             val intent = Intent(this, AuthActivity::class.java)
             startActivity(intent)
             finish()
@@ -56,5 +47,17 @@ class MainActivity : AppCompatActivity() {
             this.supportActionBar?.hide()
         } catch (e: NullPointerException) {
         }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("MainActivity", "Fetching FCM registration token failed", task.exception)
+                return@addOnCompleteListener
+            }
+
+            val token = task.result
+            Log.d("MainActivity", "FCM registration token: $token")
+        }
+
     }
+
 }
